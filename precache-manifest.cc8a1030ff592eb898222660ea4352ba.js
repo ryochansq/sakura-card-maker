552c3660aac0b589d1437926b0fa2b220b194383
ryochansq <ryochansq@satouryoutanoMacBook-Air.local>
2020-10-10 00:53:48 +0900

    Updates

---
+++ precache-manifest.cc8a1030ff592eb898222660ea4352ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e050b6b5eaf727918e855a3e168b00f7",
+    "url": "/sakura-card-maker/index.html"
+  },
+  {
+    "revision": "0c964ae128f549d40ac7",
+    "url": "/sakura-card-maker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5513cb29b85e2fa98534",
+    "url": "/sakura-card-maker/static/js/2.92533df5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sakura-card-maker/static/js/2.92533df5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c964ae128f549d40ac7",
+    "url": "/sakura-card-maker/static/js/main.e10e902a.chunk.js"
+  },
+  {
+    "revision": "933d0bf5eceb96ea788d",
+    "url": "/sakura-card-maker/static/js/runtime-main.682e95ef.js"
+  }
+]);
