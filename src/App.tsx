a4ec08e9e2f1ef90459ccfe5a60804194e466646
ryochansq <ryochansq@satouryoutanoMacBook-Air.local>
2020-10-10 01:55:11 +0900

    add font of GenEiLateMin_v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import './App.css'
 
 function App() {
   return (
-    <Button variant='contained' color='primary'>
-      Hello Sakura Gakuin!!
-    </Button>
+    <div className='App'>
+      <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+      <Button id='hoge' variant='contained' color='primary'>
+        Hello Sakura Gakuin!!
+      </Button>
+    </div>
   )
 }
 
