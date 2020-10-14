a164575543e15d92bda8a826be6c40f437df69a1
ryochansq <ryochansq@satouryoutanoMacBook-Air.local>
2020-10-15 01:33:44 +0900

    use HashRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 import TopPage from 'pages/TopPage'
 import EditPage from 'pages/EditPage'
 
 const Router = () => {
+  console.info(process.env.PUBLIC_URL)
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
-        <Route exact={true} path='/' component={TopPage} />
+        <Route path='/' exact component={TopPage} />
         <Route path='/edit' component={EditPage} />
         {/* Not Found */}
         <Route component={() => <Redirect to='/' />} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
